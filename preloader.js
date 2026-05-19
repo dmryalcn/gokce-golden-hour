dcd2917bd8540294de4cd2d7db980e580276bd8c
dmryalcn <demiryalcn10@gmail.com>
2026-05-19 23:39:55 +0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,1 +1,45 @@
+document.addEventListener("DOMContentLoaded",()=>{
 
+  /* CREATE PRELOADER */
+
+  const preloader = document.createElement("div");
+
+  preloader.id = "preloader";
+
+  preloader.innerHTML = `
+
+    <div class="preloader-glow"></div>
+
+    <div class="preloader-logo">
+      G&Y
+    </div>
+
+    <div class="preloader-line"></div>
+
+    <div class="preloader-text">
+      yükleniyor
+    </div>
+
+  `;
+
+  document.body.prepend(preloader);
+
+  /* HIDE PRELOADER */
+
+  window.addEventListener("load",()=>{
+
+    setTimeout(()=>{
+
+      preloader.classList.add("hide");
+
+      setTimeout(()=>{
+
+        preloader.remove();
+
+      },1000);
+
+    },1600);
+
+  });
+
+});
