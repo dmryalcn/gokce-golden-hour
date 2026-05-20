a558c7d3d725dff8db9afddedc44ccf67f958d05
dmryalcn <demiryalcn10@gmail.com>
2026-05-20 11:07:54 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,19 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
 
-getFirestore,
-collection,
-addDoc,
-getDocs,
-onSnapshot,
-serverTimestamp,
-query,
-orderBy
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  serverTimestamp,
+  query,
+  orderBy,
+  doc,
+  updateDoc,
+  deleteDoc,
+  where,
+  limit
 
 }
 
@@ -18,23 +23,23 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
 
-apiKey:
-"AIzaSyAvp8zij9FF7Jmae0inb3mKBie1ZrW8Dzs",
+  apiKey:
+  "AIzaSyAvp8zij9FF7Jmae0inb3mKBie1ZrW8Dzs",
 
-authDomain:
-"gokceyalcin-7d5a2.firebaseapp.com",
+  authDomain:
+  "gokceyalcin-7d5a2.firebaseapp.com",
 
-projectId:
-"gokceyalcin-7d5a2",
+  projectId:
+  "gokceyalcin-7d5a2",
 
-storageBucket:
-"gokceyalcin-7d5a2.firebasestorage.app",
+  storageBucket:
+  "gokceyalcin-7d5a2.firebasestorage.app",
 
-messagingSenderId:
-"948754842189",
+  messagingSenderId:
+  "948754842189",
 
-appId:
-"1:948754842189:web:6850d52f763f333715d204"
+  appId:
+  "1:948754842189:web:6850d52f763f333715d204"
 
 };
 
@@ -44,16 +49,71 @@ initializeApp(firebaseConfig);
 const db =
 getFirestore(app);
 
+/* GLOBAL ACCESS */
+
 window.db = db;
+
+/* FIREBASE FUNCTIONS */
 
 window.firebaseFns = {
 
-collection,
-addDoc,
-getDocs,
-onSnapshot,
-serverTimestamp,
-query,
-orderBy
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  serverTimestamp,
+  query,
+  orderBy,
+  doc,
+  updateDoc,
+  deleteDoc,
+  where,
+  limit
 
 };
+
+/* SECURITY HELPERS */
+
+window.adminSecurity = {
+
+  sessionKey:
+  "gy_admin_session",
+
+  login(password){
+
+    if(password === "GY2026SecureAdmin"){
+      
+      sessionStorage.setItem(
+        this.sessionKey,
+        "true"
+      );
+
+      return true;
+
+    }
+
+    return false;
+
+  },
+
+  logout(){
+
+    sessionStorage.removeItem(
+      this.sessionKey
+    );
+
+  },
+
+  isLoggedIn(){
+
+    return sessionStorage.getItem(
+      this.sessionKey
+    ) === "true";
+
+  }
+
+};
+
+console.log(
+  "G&Y Firebase Connected Successfully"
+);
