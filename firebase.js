92e10546b77f039f5549d6f4fed567f6e3f13e67
dmryalcn <demiryalcn10@gmail.com>
2026-05-20 10:58:30 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,35 +1,59 @@
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-  getFirestore
-} from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+getFirestore,
+collection,
+addDoc,
+getDocs,
+onSnapshot,
+serverTimestamp,
+query,
+orderBy
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
 
-  apiKey:
-  "AIzaSyAvp8zij9FF7Jmae0inb3mKBie1ZrW8Dzs",
+apiKey:
+"AIzaSyAvp8zij9FF7Jmae0inb3mKBie1ZrW8Dzs",
 
-  authDomain:
-  "gokceyalcin-7d5a2.firebaseapp.com",
+authDomain:
+"gokceyalcin-7d5a2.firebaseapp.com",
 
-  projectId:
-  "gokceyalcin-7d5a2",
+projectId:
+"gokceyalcin-7d5a2",
 
-  storageBucket:
-  "gokceyalcin-7d5a2.firebasestorage.app",
+storageBucket:
+"gokceyalcin-7d5a2.firebasestorage.app",
 
-  messagingSenderId:
-  "948754842189",
+messagingSenderId:
+"948754842189",
 
-  appId:
-  "1:948754842189:web:6850d52f763f333715d204"
+appId:
+"1:948754842189:web:6850d52f763f333715d204"
 
 };
 
 const app =
-  initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-export const db =
-  getFirestore(app);
+const db =
+getFirestore(app);
+
+window.db = db;
+
+window.firebaseFns = {
+
+collection,
+addDoc,
+getDocs,
+onSnapshot,
+serverTimestamp,
+query,
+orderBy
+
+};
